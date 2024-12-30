6ed60dbd3715a980df262994869516b2d7beadb5
Sky-Cooper <mouadhoumada@gmail.com>
2024-12-30 01:19:42 +0100

    adding some features

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -5,10 +5,3 @@ class GraphInputSerializer(serializers.Serializer):
     noeuds = serializers.ListField(child=serializers.CharField(), required=True)
     sommets = serializers.DictField(child=serializers.IntegerField(), required=True)
     la_source = serializers.CharField(required=True)
-
-
-class GraphOutputSerializer(serializers.Serializer):
-    path_lengths = serializers.DictField(child=serializers.FloatField(), required=True)
-    paths = serializers.DictField(
-        child=serializers.ListField(child=serializers.CharField()), required=True
-    )
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import GraphInputSerializer, GraphOutputSerializer
+from .serializers import GraphInputSerializer
 
 
 stored_results = {}
@@ -15,13 +15,11 @@ class FordBellmanAPIView(APIView):
             sommets = serializer.validated_data["sommets"]
             la_source = serializer.validated_data["la_source"]
 
-            # Initialize path_lengths with float('inf') for infinity values
             path_lengths = {noeud: float("inf") for noeud in noeuds}
             path_lengths[la_source] = 0
             paths = {noeud: [] for noeud in noeuds}
             paths[la_source] = [la_source]
 
-            # Bellman-Ford algorithm
             for _ in range(len(noeuds) - 1):
                 for edge, distance_x_to_y in sommets.items():
                     x, y = edge.split(",")
@@ -32,7 +30,6 @@ class FordBellmanAPIView(APIView):
                         path_lengths[y] = path_lengths[x] + distance_x_to_y
                         paths[y] = paths[x] + [y]
 
-            # Check for negative cycles
             for edge, distance_x_to_y in sommets.items():
                 x, y = edge.split(",")
                 if (
@@ -44,7 +41,6 @@ class FordBellmanAPIView(APIView):
                         status=status.HTTP_400_BAD_REQUEST,
                     )
 
-            # Replace infinity with a JSON-compliant value (e.g., "Infinity")
             response_data = {
                 "path_lengths": {
                     key: ("Infinity" if value == float("inf") else value)
@@ -53,10 +49,6 @@ class FordBellmanAPIView(APIView):
                 "paths": paths,
             }
 
-            result_key = f"{noeuds}-{sommets}-{la_source}"
-            stored_results[result_key] = response_data
-
-            output_serializer = GraphOutputSerializer(response_data)
-            return Response(output_serializer.data, status=status.HTTP_200_OK)
+            return Response(response_data, status=status.HTTP_200_OK)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
